704f1f9ae260b9a7d4e4d3d5c364b84d1dcec272
Kieran Croft <kiecroft@gmail.com>
2023-01-11 14:55:48 +0000

    version 2 - full functionality (draw, erase, clear, adjust grid size)

--- script.js
+++ script.js
@@ -1,27 +1,88 @@
-gameSize = 16;
+let gameArea = document.getElementById('gameArea')
+let color = document.getElementById('colorPicker');
+let clearBut = document.getElementById('clear');
+let slider_value = document.getElementById("myRange");
+let opacity_value = document.getElementById("opacity");
+let drawMode = document.getElementById("drawMode");
+let eraseMode = document.getElementById("eraseMode");
 
-gameArea = document.getElementById('gameArea')
+const DEFAULT_SIZE = slider_value.value;
+const DEFAULT_MODE = 'draw';
+const DEFAULT_OPACITY = opacity_value.value;
+let currentSize = DEFAULT_SIZE;
+let currentOpacity = DEFAULT_OPACITY;
+let currentMode = DEFAULT_MODE;
 
-gameArea.style.gridTemplateColumns = `repeat(${gameSize},1fr)`
-gameArea.style.gridTemplateRows = `repeat(${gameSize},1fr)`
 
-for (let i = 0; i < gameSize * gameSize; i++) {
-    const gridElement = document.createElement('div');
-    gridElement.addEventListener('mouseenter', (e) => {
-        e.target.style.backgroundColor = 'black'
-
+function gridSetUp(currentSize) {
+    gameArea.style.gridTemplateColumns = `repeat(${currentSize},1fr)`
+    gameArea.style.gridTemplateRows = `repeat(${currentSize},1fr)`
+    for (let i = 0; i < currentSize * currentSize; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.addEventListener('mouseenter', changeColour);
+        gameArea.appendChild(gridElement)
+    }
+}
+function changeColour(e){
+    if (currentMode == 'draw') {
+        e.target.style.backgroundColor = color.value;
+        e.target.style.opacity = currentOpacity;
+    } else if (currentMode == 'erase') {
+        e.target.style.backgroundColor = 'white';
+        e.target.style.opacity = currentOpacity;
+    };
+};
+function clearGrid() {
+    gameArea.innerHTML = ''
+}
+function resetGrid() {
+    clearBut.addEventListener('click',() => {
+        clearGrid()
+        sizeSlider()
+        gridSetUp(currentSize)
     });
-    gameArea.appendChild(gridElement)
 }
 
+function sizeSlider() {
+    slider_value.addEventListener('input', () => {
+        currentSize =  slider_value.value;
+        clearGrid()
+        gridSetUp(currentSize)
+    });
+}
 
-gameArea.addEventListener('mousedown', (e) => {
-    const audio = document.querySelector("audio");
-    audio.currentTime = 0 // starts audio from beginning on each click
-    audio.loop = true;
-    audio.play()
-    e.classList.toggle('playing');
+function opacitySlider() {
+    opacity_value.addEventListener('input', () => {
+        currentOpacity =  opacity_value.value;
+        // clearGrid()
+        // gridSetUp(currentSize)
+    });
+}
+
+drawMode.addEventListener('click',() => {
+    currentMode = 'draw';
+    changeColour()
 });
+eraseMode.addEventListener('click',() => {
+    currentMode = 'erase';
+    changeColour()
+});
+
+gridSetUp(currentSize)
+resetGrid()
+sizeSlider()
+opacitySlider()
+
+
+
+
+
+
+
+
+
+
+
 
 
 // To Do List
@@ -35,3 +96,15 @@ gameArea.addEventListener('mousedown', (e) => {
 // IF POSS - looping audio
 // Ensure responsiveness
 // Bake in error handling for incorrect inputs?
+
+
+
+
+
+// gameArea.addEventListener('mousedown', (e) => {
+//     const audio = document.querySelector("audio");
+//     audio.currentTime = 0 // starts audio from beginning on each click
+//     audio.loop = true;
+//     audio.play()
+//     e.classList.toggle('playing');
+// });
