6587acf7b8da23de7564eede8abb4411389bb862
Kieran Croft <kiecroft@gmail.com>
2023-01-11 15:16:17 +0000

    code refactor for readability

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// initialises DOM objects/Elements as variables
 let gameArea = document.getElementById('gameArea')
 let color = document.getElementById('colorPicker');
 let clearBut = document.getElementById('clear');
@@ -6,6 +7,7 @@ let opacity_value = document.getElementById("opacity");
 let drawMode = document.getElementById("drawMode");
 let eraseMode = document.getElementById("eraseMode");
 
+// initialises variables for default values, and adjustable "current" values
 const DEFAULT_SIZE = slider_value.value;
 const DEFAULT_MODE = 'draw';
 const DEFAULT_OPACITY = opacity_value.value;
@@ -13,52 +15,50 @@ let currentSize = DEFAULT_SIZE;
 let currentOpacity = DEFAULT_OPACITY;
 let currentMode = DEFAULT_MODE;
 
+function changeColour(e){ // sets color based on play mode and color picker choice
+    if (currentMode === 'draw') {
+        e.target.style.backgroundColor = color.value;
+        e.target.style.opacity = currentOpacity;
+    } else if (currentMode === 'erase') {
+        e.target.style.backgroundColor = 'white';
+    };
+};
+function clearGrid() { // removes underlying grid to redraw
+    gameArea.innerHTML = ''
+};
+function resetGrid() { // manages grid draw
+    clearBut.addEventListener('click',() => {
+        clearGrid()
+        sizeSlider()
+        gridSetUp(currentSize)
+    });
+};
 
-function gridSetUp(currentSize) {
+function sizeSlider() { // adjusts size of grid dynamically (user inputted)
+    slider_value.addEventListener('input', () => {
+        currentSize = slider_value.value;
+        clearGrid()
+        gridSetUp(currentSize)
+    });
+};
+
+function opacitySlider() { // adjusts opacity dynamically (user inputted)
+    opacity_value.addEventListener('input', () => {
+        currentOpacity = opacity_value.value;
+    });
+};
+
+function gridSetUp(currentSize) { // draws the grid
     gameArea.style.gridTemplateColumns = `repeat(${currentSize},1fr)`
     gameArea.style.gridTemplateRows = `repeat(${currentSize},1fr)`
     for (let i = 0; i < currentSize * currentSize; i++) {
         const gridElement = document.createElement('div');
         gridElement.addEventListener('mouseenter', changeColour);
         gameArea.appendChild(gridElement)
-    }
-}
-function changeColour(e){
-    if (currentMode == 'draw') {
-        e.target.style.backgroundColor = color.value;
-        e.target.style.opacity = currentOpacity;
-    } else if (currentMode == 'erase') {
-        e.target.style.backgroundColor = 'white';
-        e.target.style.opacity = currentOpacity;
     };
 };
-function clearGrid() {
-    gameArea.innerHTML = ''
-}
-function resetGrid() {
-    clearBut.addEventListener('click',() => {
-        clearGrid()
-        sizeSlider()
-        gridSetUp(currentSize)
-    });
-}
 
-function sizeSlider() {
-    slider_value.addEventListener('input', () => {
-        currentSize =  slider_value.value;
-        clearGrid()
-        gridSetUp(currentSize)
-    });
-}
-
-function opacitySlider() {
-    opacity_value.addEventListener('input', () => {
-        currentOpacity =  opacity_value.value;
-        // clearGrid()
-        // gridSetUp(currentSize)
-    });
-}
-
+// event listeners to capture clicks on draw or erase buttons
 drawMode.addEventListener('click',() => {
     currentMode = 'draw';
     changeColour()
@@ -68,43 +68,8 @@ eraseMode.addEventListener('click',() => {
     changeColour()
 });
 
+// function calls to run app
 gridSetUp(currentSize)
 resetGrid()
 sizeSlider()
 opacitySlider()
-
-
-
-
-
-
-
-
-
-
-
-
-
-// To Do List
-// Refactor grid code into clean functions
-// Add user input for size
-// set up default parameters
-// add colour picker
-// add opacity
-// add erase function and clear function
-// Final styling and tweaks
-// IF POSS - looping audio
-// Ensure responsiveness
-// Bake in error handling for incorrect inputs?
-
-
-
-
-
-// gameArea.addEventListener('mousedown', (e) => {
-//     const audio = document.querySelector("audio");
-//     audio.currentTime = 0 // starts audio from beginning on each click
-//     audio.loop = true;
-//     audio.play()
-//     e.classList.toggle('playing');
-// });
